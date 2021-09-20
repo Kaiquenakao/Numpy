3f381f18930cc41f81f233ee9470257c81f1778d
kaiquenakao <kaiquenakao@outlook.com>
2021-09-20 16:58:50 -0300

    First Commit

---
+++ mp3.py
@@ -0,0 +1,19 @@
+import PySimpleGUI as sg
+import vlc
+
+def mp3():
+    sg.theme('Reddit')
+    # Definir o layout
+    layout = [
+    ]
+    return sg.Window('MP3', layout, finalize=True)
+
+# GUI
+janela = mp3()
+
+# Eventos do sistema
+while True:
+    window, event, values = sg.read_all_windows()
+    if event == sg.WINDOW_CLOSED:
+        break
+
